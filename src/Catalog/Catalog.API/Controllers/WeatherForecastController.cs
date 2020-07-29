bacebb2c4a814542adc766a6c1f3dc72c9e949b0
ertuğrul yılmaz <ertgylmz_1608@hotmail.com>
2020-07-29 12:31:47 +0300

    Test Deneme Değişiklik

--- src/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
+++ src/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Catalog.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Harsss"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
